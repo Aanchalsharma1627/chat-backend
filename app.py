f142eea9108be70689982d1a6fe18685423a16ab
Aanchal <aanchalsharma65480@gmail.com>
2025-07-04 19:39:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "✅ Flask server is running!"
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_msg = data.get('message', '')
+    user_name = data.get('user', 'User')
+
+    response = f"Hello {user_name}, you said: '{user_msg}'"
+    return jsonify({'reply': response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
